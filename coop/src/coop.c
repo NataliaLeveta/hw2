84b9e08f94b650b316b6a4ab87730d27d3dac88b
NataliaLeveta <61879991+NataliaLeveta@users.noreply.github.com>
2024-01-14 18:09:28 +0200

    Update coop.c

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -5,26 +5,26 @@ int main () {
     int r = 1; // round number
 
     while (r <= 1000000) { // Loop for each round, up to 1,000,000 rounds
-        if (r % 2 == 1) {
-            putchar ('C'); // Print 'C' for odd rounds
-            putchar('\n');
-            fflush (stdout); // Flush the output buffer to ensure immediate display
+        if (r % 2 == 1) { // if round is odd 
+            putchar ('C'); // print 'C'
+            putchar('\n'); // change line 
+            fflush (stdout); // flush the output buffer to ensure immediate display
         }
-        if (r % 2 == 0) {
-            putchar ('D'); // Print 'D' for even rounds
+        if (r % 2 == 0) { // if round is even
+            putchar ('D'); // print 'D'
             putchar('\n');
             fflush (stdout);
         }
 
-        int re;
+        char move; // enemy move
         re = getchar();
 
         while(re != 'C' && re != 'D' && re != EOF) {
-            re = getchar(); // Wait for user input ('C', 'D', or EOF)
+            re = getchar(); // wait for user input ('C', 'D', or EOF)
         }
 
-        if (re == EOF) break; // If End of File is encountered, exit the loop
+        if (re == EOF) break; // if End Of File then exit the loop
 
-        r++; // Move to the next round
+        r++; // move to the next round
     }
 }
